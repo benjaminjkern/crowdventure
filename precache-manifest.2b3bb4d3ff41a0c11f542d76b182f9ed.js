b6ab2d4b8942b4c80431705e38e47e5ec1849a08
benjaminjkern <bkern1@lion.lmu.edu>
2020-07-31 17:50:38 -0700

    Updates

--- precache-manifest.2b3bb4d3ff41a0c11f542d76b182f9ed.js
+++ precache-manifest.2b3bb4d3ff41a0c11f542d76b182f9ed.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e0309eadd43917691e0408882ab7d7c2",
+    "revision": "0db99967a50db0d08a8b8f278448a044",
     "url": "/crowdventure/index.html"
   },
   {
-    "revision": "4834d3f55c982fea4b30",
+    "revision": "8534848ae210a7ef938f",
     "url": "/crowdventure/static/css/2.afe213f9.chunk.css"
   },
   {
-    "revision": "4834d3f55c982fea4b30",
-    "url": "/crowdventure/static/js/2.74ac4ad0.chunk.js"
+    "revision": "8534848ae210a7ef938f",
+    "url": "/crowdventure/static/js/2.06133c83.chunk.js"
   },
   {
     "revision": "f2979e099ee4c06ef4b08e1c5db2b1ac",
-    "url": "/crowdventure/static/js/2.74ac4ad0.chunk.js.LICENSE.txt"
+    "url": "/crowdventure/static/js/2.06133c83.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6253f77f69f6e1f4f51b",
-    "url": "/crowdventure/static/js/main.d028dd3b.chunk.js"
+    "revision": "7ffc83623af8c1872c77",
+    "url": "/crowdventure/static/js/main.56262ed0.chunk.js"
   },
   {
     "revision": "fcbe28f15a7ea9dcd9f5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crowdventure/precache-manifest.5d7c26d156f34290acc7dc85c92928d4.js"
+  "/crowdventure/precache-manifest.2b3bb4d3ff41a0c11f542d76b182f9ed.js"
 );
 
 self.addEventListener('message', (event) => {
