f08369bd38f6101bc5ab8ce2b8cab5e9c9507ee7
benjaminjkern <bkern1@lion.lmu.edu>
2020-09-16 21:29:08 -0700

    Updates

---
+++ precache-manifest.7b226272610945c9e4dc447e30c72ab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a7018cb8f23e6609075198ebfedca1",
+    "url": "/crowdventure/index.html"
+  },
+  {
+    "revision": "60c6f993f56ecde4ff6c",
+    "url": "/crowdventure/static/css/2.90551392.chunk.css"
+  },
+  {
+    "revision": "60c6f993f56ecde4ff6c",
+    "url": "/crowdventure/static/js/2.2c8589bd.chunk.js"
+  },
+  {
+    "revision": "f2979e099ee4c06ef4b08e1c5db2b1ac",
+    "url": "/crowdventure/static/js/2.2c8589bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00a9f9b3999e663b6a1",
+    "url": "/crowdventure/static/js/main.9e0233e1.chunk.js"
+  },
+  {
+    "revision": "815a856a2be7775cc4f6",
+    "url": "/crowdventure/static/js/runtime-main.2b50bfaa.js"
+  }
+]);
