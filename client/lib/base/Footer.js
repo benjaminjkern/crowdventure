a9f7a8acb6d7db8070dee835efab91d3f9ff8eb6
benjaminjkern <benkern@pixelgate.net>
2023-09-23 10:06:49 -0700

    SMall changes I never pushed up

--- client/lib/base/Footer.js
+++ client/lib/base/Footer.js
@@ -5,7 +5,7 @@ import packageJson from "../../package.json";
 const Footer = () => {
     return (
         <>
-            @ 2022 Copyright: (MIT) Benjamin Kern
+            © 2022 Copyright: (MIT) Benjamin Kern
             <Link href="https://github.com/benjaminjkern/crowdventure/blob/master/CHANGELOG.md">
                 <a>Version: {packageJson.version}</a>
             </Link>
