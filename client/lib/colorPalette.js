104b8e06e158e7d11b920ff9502afd6bfbd12a0a
benjaminjkern <benkern@pixelgate.net>
2023-11-30 10:41:23 -0800

    Handle background a bit nicer

--- client/lib/colorPalette.js
+++ client/lib/colorPalette.js
@@ -4,9 +4,22 @@ import { ThemeProvider, createUseStyles } from "react-jss";
 
 export const PaletteContext = createContext();
 
+const getPalette = (unsafeMode) => ({
+    rootColor: [
+        "rgb(80, 160, 224)",
+        "rgb(100, 180, 244)",
+        "rgb(158, 232, 255)",
+    ],
+    textColor: unsafeMode ? "white" : "black",
+    backgroundColor: unsafeMode
+        ? ["rgb(34,34,34)", "rgb(41,41,41)", "rgb(50,50,50)"]
+        : ["rgb(255,255,255)", "rgb(220,245,255)", "rgb(180,238,255)"],
+    borderColor: "lightgray",
+});
+
 const useDefaultStyles = createUseStyles((theme) => {
     // Use theme basically just so react-jss doesnt get mad at me
-    const { rootColor } = theme;
+    const { rootColor, backgroundColor, textColor } = theme;
     return {
         "@global": {
             /** ***************** Custom Selection ************************/
@@ -27,21 +40,18 @@ const useDefaultStyles = createUseStyles((theme) => {
             "a:focus, a:hover": {
                 textDecoration: "underline",
             },
+            body: {
+                backgroundImage: `linear-gradient(
+                    to right,
+                    rgb(158, 232, 255),
+                    ${backgroundColor[0]} 10%,
+                    ${backgroundColor[0]} 90%,
+                    rgb(158, 232, 255)
+                )`,
+                color: textColor,
+            },
         },
     };
-});
-
-const getPalette = (unsafeMode) => ({
-    rootColor: [
-        "rgb(80, 160, 224)",
-        "rgb(100, 180, 244)",
-        "rgb(158, 232, 255)",
-    ],
-    textColor: unsafeMode ? "white" : "black",
-    backgroundColor: unsafeMode
-        ? ["rgb(34,34,34)", "rgb(41,41,41)", "rgb(50,50,50)"]
-        : ["rgb(255,255,255)", "rgb(220,245,255)", "rgb(180,238,255)"],
-    borderColor: "lightgray",
 });
 
 const PaletteProvider = ({ children }) => {
@@ -50,16 +60,7 @@ const PaletteProvider = ({ children }) => {
     const [palette, setPalette] = useState(getPalette(unsafeMode));
 
     useEffect(() => {
-        const newPalette = getPalette(unsafeMode);
-        document.body.style.backgroundImage = `linear-gradient(
-            to right,
-            rgb(158, 232, 255),
-            ${newPalette.backgroundColor[0]} 10%,
-            ${newPalette.backgroundColor[0]} 90%,
-            rgb(158, 232, 255)
-        )`;
-        document.body.style.color = newPalette.textColor;
-        setPalette(newPalette);
+        setPalette(getPalette(unsafeMode));
     }, [unsafeMode]);
 
     return (
