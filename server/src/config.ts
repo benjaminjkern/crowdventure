1d8e259fd8300895da2c4ed78d9b713a17ffe7cd
benjaminjkern <benkern@pixelgate.net>
2024-01-23 14:08:12 -0800

    woopsies

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,7 +3,7 @@ import { createConfig } from "express-zod-api";
 
 export const config = createConfig({
     server: {
-        listen: process.env.NODE_ENV === "production" ? 80 : 4000,
+        listen: process.env.NODE_ENV === "devlopment" ? 4000 : 80,
         upload: true,
         // TODO: IDK WHAT THIS IS
         compression: true, // affects sendAvatarEndpoint
