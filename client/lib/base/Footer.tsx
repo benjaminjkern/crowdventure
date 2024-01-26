9d5c4459a0ea7269dc3144d5d1cf8dbc307c884f
benjaminjkern <benkern@pixelgate.net>
2024-01-26 14:51:43 -0800

    Buy me a coffee

--- client/lib/base/Footer.tsx
+++ client/lib/base/Footer.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import Link from "next/link";
+import { PaletteContext } from "../colorPalette";
 
-const Footer = () => (
-    <footer
-        style={{
-            flexDirection: "row",
-            padding: 30,
-            justifyContent: "space-between",
-        }}
-    >
-        © 2022 Copyright: (MIT) Benjamin Kern
-        <Link href="https://github.com/benjaminjkern/crowdventure/blob/master/CHANGELOG.md">
-            Version: {process.env.npm_package_version}
-        </Link>
-    </footer>
-);
+const Footer = () => {
+    const { mutedTextColor } = useContext(PaletteContext);
+    return (
+        <footer
+            style={{
+                flexDirection: "row",
+                padding: 30,
+                justifyContent: "space-between",
+            }}
+        >
+            <Link href="https://www.buymeacoffee.com/benkern">
+                Buy me a coffee!
+            </Link>
+            <span style={{ color: mutedTextColor }}>
+                © 2022 Copyright: (MIT) Crowdventure
+            </span>
+            <Link href="https://github.com/benjaminjkern/crowdventure/blob/master/CHANGELOG.md">
+                Version: {process.env.npm_package_version}
+            </Link>
+        </footer>
+    );
+};
 
 export default Footer;
