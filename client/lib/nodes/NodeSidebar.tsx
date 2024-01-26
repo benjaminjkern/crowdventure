9136514eaef3e98792250a7d16ee866c43339744
benjaminjkern <benkern@pixelgate.net>
2024-01-26 02:05:01 -0800

    Clean up the whole way the app works on mobile mode so it's a bit nicer to use

--- client/lib/nodes/NodeSidebar.tsx
+++ client/lib/nodes/NodeSidebar.tsx
@@ -1,4 +1,9 @@
-import React, { type Dispatch, type SetStateAction, useContext } from "react";
+import React, {
+    type Dispatch,
+    type SetStateAction,
+    useContext,
+    type ReactNode,
+} from "react";
 import { useRouter } from "next/router";
 import CrowdventureButton from "../components/CrowdventureButton";
 import { UserContext } from "../user";
@@ -13,6 +18,21 @@ import GoogleAdList from "../components/GoogleAdList";
 import { EditNodeModal } from "./NodeModal";
 import { type Choice, type Node } from "@/types/models";
 
+const EmptyText = ({ children }: { readonly children: ReactNode }) => {
+    const { mutedTextColor } = useContext(PaletteContext);
+    return (
+        <span
+            style={{
+                color: mutedTextColor,
+                textAlign: "center",
+                marginBottom: 10,
+            }}
+        >
+            {children}
+        </span>
+    );
+};
+
 const NodeSidebar = ({
     node,
     setNode,
@@ -26,41 +46,18 @@ const NodeSidebar = ({
 }) => {
     const { user } = useContext(UserContext);
     const { openModal } = useContext(ModalContext);
-    const { mutedTextColor } = useContext(PaletteContext);
 
     const isMobile = useMediaQuery("(max-width: 800px)");
 
     const canonChoices = choices.filter((choice) => choice.isCanon);
+    const nonCanonChoices = choices.filter((choice) => !choice.isCanon);
 
     const router = useRouter();
 
     const loggedInAsOwner = node.ownerId === user?.id;
-    return (
-        <div>
-            <div
-                style={{
-                    gap: 20,
-                    margin: 20,
-                    marginBottom: 15,
-                    marginInline: isMobile ? 20 : 0,
-                }}
-            >
-                {!isMobile && (
-                    <h1 style={{ textAlign: "center", display: "block" }}>
-                        {node.title}
-                    </h1>
-                )}
 
-                <ParagraphText text={node.content} />
-
-                {canonChoices.length === 0 && (
-                    <span style={{ color: mutedTextColor }}>
-                        By decree of {node.owner?.screenName ?? "Crowdventure"},
-                        this journey ends here.
-                    </span>
-                )}
-            </div>
-            <hr />
+    const infoAndButtons = (
+        <div style={{ marginBlock: 10 }}>
             <div
                 style={{
                     flexDirection: "row",
@@ -113,15 +110,19 @@ const NodeSidebar = ({
                     Suggest New Choice
                 </CrowdventureButton>
             </div>
-            <hr />
-            {choices.length > 0 ? (
+        </div>
+    );
+
+    const choiceList = (list: Choice[], backup: string) => {
+        if (list.length > 0)
+            return (
                 <GoogleAdList
                     columnsCountBreakPoints={
                         node.pictureURL
                             ? { 0: 1, 1400: 2 }
                             : { 0: 1, 600: 2, 900: 3 }
                     }
-                    data={choices}
+                    data={list}
                     render={(choice, idx) => (
                         <ChoiceCard
                             choice={choice}
@@ -146,17 +147,71 @@ const NodeSidebar = ({
                         />
                     )}
                 />
+            );
+
+        return <EmptyText>{backup}</EmptyText>;
+    };
+
+    return (
+        <div>
+            <div
+                style={{
+                    gap: 20,
+                    margin: 20,
+                    marginBottom: 18,
+                    marginInline: isMobile ? 20 : 0,
+                }}
+            >
+                {!isMobile && (
+                    <h1 style={{ textAlign: "center", display: "block" }}>
+                        {node.title}
+                    </h1>
+                )}
+
+                <ParagraphText text={node.content} />
+            </div>
+            {isMobile ? (
+                <>
+                    {choiceList(
+                        canonChoices,
+                        choices.length > 0
+                            ? `By decree of ${
+                                  node.owner?.screenName ?? "Crowdventure"
+                              },
+                        this journey ends here.`
+                            : ""
+                    )}
+                    {canonChoices.length === 0 && <hr />}
+                    {infoAndButtons}
+                    {choices.length === 0 && (
+                        <EmptyText>
+                            There is currently nowhere to go from here! You can
+                            help expand the story by adding to it!
+                        </EmptyText>
+                    )}
+                    {nonCanonChoices.length ? (
+                        <>
+                            <hr />
+                            <span style={{ marginBottom: 5 }}>
+                                Other Choices:
+                            </span>
+
+                            {choiceList(
+                                nonCanonChoices,
+                                "This shouldnt be showing!"
+                            )}
+                        </>
+                    ) : null}
+                </>
             ) : (
-                <span
-                    style={{
-                        color: mutedTextColor,
-                        textAlign: "center",
-                        marginBottom: 10,
-                    }}
-                >
-                    There are currently no options! You can help expand the
-                    story by adding to it!
-                </span>
+                <>
+                    <hr />
+                    {infoAndButtons}
+                    {choiceList(
+                        [...canonChoices, ...nonCanonChoices],
+                        "There is currently nowhere to go from here! You can help expand the story by adding to it!"
+                    )}
+                </>
             )}
         </div>
     );
