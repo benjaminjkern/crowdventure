22440c9ab454c89d390d3641231bb63fbbf1e6fb
benjaminjkern <benkern@pixelgate.net>
2024-01-26 02:12:44 -0800

    Small fix

--- client/lib/nodes/NodeSidebar.tsx
+++ client/lib/nodes/NodeSidebar.tsx
@@ -149,7 +149,7 @@ const NodeSidebar = ({
                 />
             );
 
-        return <EmptyText>{backup}</EmptyText>;
+        if (backup) return <EmptyText>{backup}</EmptyText>;
     };
 
     return (
