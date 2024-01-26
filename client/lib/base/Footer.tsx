e55c0f56aa60f7b19e697078b74273d15dfe98b3
benjaminjkern <benkern@pixelgate.net>
2024-01-26 15:00:00 -0800

    Fixed margins on footer

--- client/lib/base/Footer.tsx
+++ client/lib/base/Footer.tsx
@@ -8,17 +8,26 @@ const Footer = () => {
         <footer
             style={{
                 flexDirection: "row",
-                padding: 30,
+                paddingBlock: 30,
                 justifyContent: "space-between",
+                gap: 10,
             }}
         >
-            <Link href="https://www.buymeacoffee.com/benkern">
+            <Link
+                href="https://www.buymeacoffee.com/benkern"
+                style={{ flex: 1, textAlign: "center" }}
+            >
                 Buy me a coffee!
             </Link>
-            <span style={{ color: mutedTextColor }}>
+            <span
+                style={{ flex: 1, color: mutedTextColor, textAlign: "center" }}
+            >
                 © 2022 Copyright: (MIT) Crowdventure
             </span>
-            <Link href="https://github.com/benjaminjkern/crowdventure/blob/master/CHANGELOG.md">
+            <Link
+                href="https://github.com/benjaminjkern/crowdventure/blob/master/CHANGELOG.md"
+                style={{ flex: 1, textAlign: "center" }}
+            >
                 Version: {process.env.npm_package_version}
             </Link>
         </footer>
